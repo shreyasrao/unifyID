c455b9660ba96489ae0dcc5658ae5498e7b836ca
shreyasrao <s23rao@gmail.com>
2017-08-22 09:54:09 -0700

    initial commit of online random used to get random numbers from random.org

---
+++ randomORG.py
@@ -0,0 +1,21 @@
+import math
+import random
+import urllib.request
+import time
+
+def onlineRandom(low=0,high=255,num=10):
+    url = 'https://www.random.org/integers/?num={}&min={}&max={}&col=1&base=10&format=plain&rnd=new'.format(num,low,high)
+
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+
+    html = html.decode('utf8').splitlines()
+    ints = [int(x) for x in html]
+
+    return ints
+
+numbers = onlineRandom()
+
+print(numbers)
+print(type(numbers[0]))
+print(len(numbers))
